5c113d93fdbc77686a8afb6fa1270763461069a3
Nguyễn Hoàng Minh Tú <71559192+Bynn-1401-Git@users.noreply.github.com>
2022-05-25 09:38:02 +0700

    improvement source code

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "Switching to branch master"
+echo "Switching to branch master..."
 git checkout main
+echo "Fetching branch..."
+git fetch origin
 echo "Pulling latest changes"
 git pull
 echo "install node_modules"
-npm install --legacy-peer-deps
+npm install
 echo "Building app"
 npm run build
 echo "Deployment complete"
