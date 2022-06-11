ce1d32d5594e0dcd5e3cfdb3218763ef9b7d83fb
Nguyễn Hoàng Minh Tú <71559192+Bynn-1401-Git@users.noreply.github.com>
2022-06-11 16:26:47 +0700

    final config

--- src/index.js
+++ src/index.js
@@ -4,22 +4,20 @@ import 'react-app-polyfill/stable';
 import 'core-js/es/map';
 import 'core-js/es/set';
 import 'raf/polyfill';
-import React, {useEffect} from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider, ReactReduxContext } from 'react-redux';
 import { ConfigProvider } from 'antd';
 import vi_VN from 'antd/lib/locale/vi_VN';
 import configureAppStore from './app/store';
 import 'moment/locale/vi';
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 import { history } from '@utils/index';
 import App from './app';
 import './styles/base/reset.css';
 import './styles/base/app.less';
-import './styles/index.css';
-import 'react-toastify/dist/ReactToastify.css';
-
-
+import './styles/index.scss';
+// import 'react-toastify/dist/ReactToastify.css';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
@@ -27,22 +25,7 @@ const container = document.getElementById('root');
 const store = configureAppStore();
 const root = createRoot(container);
 
-// toast.configure({
-//   position: "top-right",
-//   autoClose: 5000,
-//   hideProgressBar: false,
-//   closeOnClick: true,
-//   pauseOnHover: true,
-//   draggable: true,
-//   progress: undefined,
-//   limit:3
-// });
-
-
 function AppWithCallbackAfterRender() {
-  useEffect(() => {
-    console.log('rendered');
-  });
 
   return (<React.StrictMode>
     <ConfigProvider locale={vi_VN}>
