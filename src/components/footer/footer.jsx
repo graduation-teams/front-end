aba293e58dfe76badce97a0fd2efdfcddd82e50e
Mikey <89092556+ntquan612@users.noreply.github.com>
2022-06-12 18:10:43 +0700

    create footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,160 @@
 import React from 'react';
 
-function Footer(props) {
-    return (
-        <div className='text-black text-[25px] text-center'>
-            This is footer
-        </div>
-    );
-}
+import Icon from '@ant-design/icons';
+import { Layout, Row, Col, BackTop } from 'antd';
 
-export default Footer;
+import { ReactComponent as IconLogo1 } from '@assets/images/footer-1.svg';
+
+const FooterComponent = () => {
+    return (
+        <Layout.Footer style={{ padding: 0, minHeight: '200px' }}>
+            <section className="footer">
+                <div className="footer__header">
+                    <div className="container-1200">
+                        <Row>
+                            <Col sm={24} md={8} xl={8}>
+                                <div className="footer-support">
+                                    <Icon component={IconLogo1} />
+                                    <div>
+                                        <h1 className="title">CALL US 24/7</h1>
+                                        <p className="desc">+1 (310) 888 8808, +1 (310) 777 8808</p>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col sm={24} md={8} xl={8}>
+                                <div className="footer-support">
+                                    <Icon component={IconLogo1} />
+                                    <div>
+                                        <h1 className="title">WRITE TO US</h1>
+                                        <p className="desc">electon.@gm.com , electron@gm.com</p>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col sm={24} md={8} xl={8}>
+                                <div className="footer-support">
+                                    <Icon component={IconLogo1} />
+                                    <div>
+                                        <h1 className="title">CONTACT INFO</h1>
+                                        <p className="desc">USA, New York, Second Street 289</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </section>
+
+            <section className="container-1200">
+                <div className="footer__mid">
+                    <Row>
+                        <Col sm={24} md={7} xl={7} className="footer__mid--columns">
+                            <h1 className="text">Sign up to Our Newsletter</h1>
+                        </Col>
+                        <Col sm={24} md={10} xl={10} className="footer__mid--columns">
+                            <div className="input-signup">
+                                <input type="text" className="text-signup" placeholder="Nhập email của bạn tại đây" />
+                                <button className="btn-signup">SIGN UP</button>
+                            </div>
+                        </Col>
+                        <Col sm={24} md={7} xl={7} className="footer__mid--columns">
+                            <div className="icon-social-media">
+                                <Icon component={IconLogo1} />
+                                <Icon component={IconLogo1} />
+                                <Icon component={IconLogo1} />
+                                <Icon component={IconLogo1} />
+                                <Icon component={IconLogo1} />
+                                <Icon component={IconLogo1} />
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
+            </section>
+
+            <section className="container-1200">
+                <div className="footer__bot">
+                    <Row>
+                        <Col xs={24} md={12} xl={6} className="footer__bot--columns">
+                            <h1 className="text">ELECTRON 01</h1>
+                            <hr />
+                            <ul>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col xs={24} md={12} xl={6} className="footer__bot--columns">
+                            <h1 className="text">ELECTRON 01</h1>
+                            <hr />
+                            <ul>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col xs={24} md={12} xl={6} className="footer__bot--columns">
+                            <h1 className="text">ELECTRON 01</h1>
+                            <hr />
+                            <ul>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col xs={24} md={12} xl={6} className="footer__bot--columns">
+                            <h1 className="text">ELECTRON 01</h1>
+                            <hr />
+                            <ul>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                                <li className="item">
+                                    <a href="#">Nguyen Tinh Quan</a>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </div>
+                <hr />
+            </section>
+
+            <div className="container-1200">
+                <p>Copyright © 2021 XStore Theme. Created by 8theme - Premium WooCommerce Themes.</p>
+            </div>
+        </Layout.Footer>
+    );
+};
+
+export default FooterComponent;
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,40 +1,39 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { connect } from 'react-redux'
-import { Layout,Menu } from 'antd';
+import { connect } from 'react-redux';
+import { Layout, Menu } from 'antd';
+import FooterComponent from '../components/footer/footer';
 
-function Layouts({ childrenComponent = []}){
+function Layouts({ childrenComponent = [] }) {
+  const [components, setComponents] = useState([]);
 
-   const [components, setComponents] = useState([]);
+  useEffect(() => {
+    if (childrenComponent.length > 0) {
+      setComponents(childrenComponent);
+    }
+  }, [childrenComponent]);
 
-    useEffect(()=>{
-        if(childrenComponent.length > 0){
-            setComponents(childrenComponent);
-        }
-    },[childrenComponent])
-
-    return (
-        <React.Fragment>
-            <Layout style={typeof window === 'undefined' ? { display: 'none' } : {backgroundColor:'#ffffff'}}>
-            
-                {/* <HeaderComponent /> */}
-                <Layout.Content>
-                {
-                    components && components.length>0? components.map(({element: Element}, index) => {
-                        return(<Element key={index}/>)
-                    }): null
-                }
-                </Layout.Content>
-                {/* <FooterComponent /> */}
-            </Layout>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Layout style={typeof window === 'undefined' ? { display: 'none' } : { backgroundColor: '#ffffff' }}>
+        {/* <HeaderComponent /> */}
+        <Layout.Content>
+          {components && components.length > 0
+            ? components.map(({ element: Element }, index) => {
+                return <Element key={index} />;
+              })
+            : null}
+        </Layout.Content>
+        <FooterComponent />
+      </Layout>
+    </React.Fragment>
+  );
 }
 
 const mapStateToProps = state => ({
-    pathname: state.router.location.pathname,
-    search: state.router.location.search,
-    hash: state.router.location.hash,
-})
+  pathname: state.router.location.pathname,
+  search: state.router.location.search,
+  hash: state.router.location.hash,
+});
 
 export default connect(mapStateToProps)(Layouts);
